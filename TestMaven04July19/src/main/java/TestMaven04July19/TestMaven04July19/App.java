6bb872269c61a0001f9949db3bf7efcf97e03dd9
vigneshkrishnamoorthy <42338477+vigneshkrishnamoorthy@users.noreply.github.com>
2019-07-04 10:28:26 +0530

    Added 'TestMaven04July19' on 04 July 2019.

---
+++ TestMaven04July19/src/main/java/TestMaven04July19/TestMaven04July19/App.java
@@ -0,0 +1,13 @@
+package TestMaven04July19.TestMaven04July19;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
